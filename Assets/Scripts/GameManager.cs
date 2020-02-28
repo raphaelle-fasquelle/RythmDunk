9ad8efeb26fa3cc6531131c10cb140ed7ae52f7e
Raphaëlle Fasquelle <raphaelle.fasquelle@gmail.com>
2020-02-28 19:53:02 +0100

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Canvases")]
+    public GameObject inGameCanvas;
+    public GameObject winCanvas;
+    public GameObject loseCanvas;
+    public GameObject menuCanvas;
+
+    private void Start()
+    {
+    }
+
+    public void ChangeUIState(string state)
+    {
+        switch (state)
+        {
+            case "InGame":
+                SetActiveOrInactive(new List<GameObject> { inGameCanvas }, new List<GameObject> { winCanvas, loseCanvas, menuCanvas });
+                break;
+            case "Win":
+                SetActiveOrInactive(new List<GameObject> { winCanvas }, new List<GameObject> { inGameCanvas, loseCanvas, menuCanvas });
+                break;
+            case "Lose":
+                SetActiveOrInactive(new List<GameObject> { loseCanvas }, new List<GameObject> { winCanvas, inGameCanvas, menuCanvas });
+                break;
+            case "Menu":
+                SetActiveOrInactive(new List<GameObject> { menuCanvas }, new List<GameObject> { winCanvas, loseCanvas, inGameCanvas });
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SetActiveOrInactive(List<GameObject> activObjs, List<GameObject> inactivObjs)
+    {
+        foreach(GameObject go in activObjs)
+            go.SetActive(true);
+        foreach(GameObject go in inactivObjs)
+            go.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/MusicInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+[System.Serializable]
+public class MusicInfo : MonoBehaviour
+{
+    string path, jsonString;
+    public static List<float> startTimes;
+    public static float musicDuration;
+
+    private void Start()
+    {
+        path = Application.streamingAssetsPath + "/tfr_unity_1m14s_jeu_3.glm.bytes";
+        jsonString = File.ReadAllText(path);
+        GPGameLevelMakerFile musicEvents = JsonUtility.FromJson<GPGameLevelMakerFile>(jsonString);
+        startTimes = new List<float>();
+
+        foreach(GPGameEvent gpe in musicEvents.events)
+        {
+            startTimes.Add(gpe.startTime);
+        }
+        musicDuration = musicEvents.duration;
+    }
+
+    [Serializable]
+    public class GPGameEvent
+    {
+        public float startTime;
+        public float endTime;
+        public int type;
+        public string typeParameters;
+    }
+
+    [Serializable]
+    public class GPGameLevelMakerFile
+    {
+        public float bpm;
+        public float duration;
+        public List<GPGameEvent> events;
+    }
+}
